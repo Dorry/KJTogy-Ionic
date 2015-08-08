06ee215c428068ff4a9e04a69b7bc3160f9ea525
Dorry <dorry457@nate.com>
2015-08-09 05:28:24 +0900

    ㅠㅠㅠㅠㅠㅠ

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -304,7 +304,7 @@ angular.module('kjtogy.controllers', [])
 
     $scope.header_view = true;
 
-    $scope.image = "data:image/jpeg;base64,"+$scope.data;
+    $scope.image = "data:image/jpg;base64,"+$scope.data;
 
     $scope.displayHeader = function() {
         $scope.header_view = !$scope.header_view;
